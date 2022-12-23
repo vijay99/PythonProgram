8c5fdf4abba0f95a888ae277a699d2c877351a3b
Kumar Vijay <vijay81238@gmail.com>
2022-12-23 16:44:33 +0530

    First commit

---
+++ Demo.py
@@ -0,0 +1,5 @@
+
+print("Hello Python--My First python program in Pycharm!")
+n = 10
+for i in range(n):
+    print(i)
---
+++ sorting/bubble_sort.py
@@ -0,0 +1,21 @@
+def bubble_sort(arr):
+    temp = 0
+    is_sorted = False
+
+    for i in range(len(arr)-1):
+        is_sorted = True
+        for j in range(len(arr) - 1 - i):
+            if arr[j] > arr[j+1]:
+                is_sorted = False
+                temp = arr[j+1]
+                arr[j+1] = arr[j]
+                arr[j] = temp
+        if is_sorted:
+            break
+
+
+# Driver class
+arr = [7, 9, 1, 0, 6]
+bubble_sort(arr)
+for i in range(len(arr)):
+    print(arr[i])
